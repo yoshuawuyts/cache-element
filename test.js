e405d70244a8a366cb4519b18f02e1dc58394105
jekrb <Jacob.JW.Burden@gmail.com>
2016-10-07 00:45:30 -0400

    clean up for linting

--- test.js
+++ test.js
@@ -5,12 +5,11 @@ test('cache', function (t) {
   var render = cacheElement(function (el) {
     return el
   })
-  
+
   t.same(render('alice'), 'alice')
   t.same(render('alice'), 'alice')
   t.same(render('bob'), 'bob')
 })
-
 
 test('should assert input types', function (t) {
   t.plan(1)
