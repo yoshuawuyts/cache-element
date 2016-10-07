8d3d6b757a0a452f3a7a60cf9b1c9c2aaae272bf
jekrb <Jacob.JW.Burden@gmail.com>
2016-10-07 00:42:22 -0400

    clean up for linting

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const assert = require('assert')
-
 module.exports = cacheElement
 
 function cacheElement (fn) {
   const store = {}
-  
+
   return function render () {
     const args = Array.from(arguments)
     const argsAreTheSame = JSON.stringify(store.prev) === JSON.stringify(args)
-    
+
     if (argsAreTheSame) return store.el
-    
+
     store.prev = args
     store.el = fn.apply(this, args)
     return store.el
   }
 }
-
 
