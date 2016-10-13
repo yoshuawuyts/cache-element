cb313a35afba2b38ae2384b4c9d3482f3a7dff26
Bjorn <bjorn.roberg@gmail.com>
2016-10-13 23:21:41 +0200

    adds initial implementation based on jekrb's code. Adds tests that checks memoization is working as expected

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+'use strict'
+
+const assert = require('assert')
 module.exports = cacheElement
 
+const __slice = Function.prototype.call.bind(Array.prototype.slice)
+
 function cacheElement (fn) {
+  assert(typeof fn === 'function', 'cacheElement accepts one argument, which must be a `function`')
   const store = {}
 
   return function render () {
-    const args = Array.from(arguments)
-    const argsAreTheSame = JSON.stringify(store.prev) === JSON.stringify(args)
+    const args = __slice(arguments, 0)
+    let argsAreTheSame = !!store.prev
+    for (let i = 0; i < (store.prev || []).length; ++i) {
+      // not `break`ing cause I don't think users will pass a million args
+      argsAreTheSame = argsAreTheSame && store.prev[i] === args[i]
+    }
 
     if (argsAreTheSame) return store.el
 
@@ -14,4 +24,3 @@ function cacheElement (fn) {
     return store.el
   }
 }
-
