4fe3799ccd63986948bbfffeee6849fce4357d1c
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-11-30 15:38:42 +0100

    feat(widget): redo

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-const cacheElement = require('./')
-const widget = require('./widget')
-const test = require('tape')
-const html = require('bel')
+var cacheElement = require('./')
+var widget = require('./widget')
+var test = require('tape')
+var html = require('bel')
 
 test('cache', (t) => {
   t.test('should validate input types', (t) => {
@@ -13,30 +13,30 @@ test('cache', (t) => {
   t.test('should render elements', (t) => {
     t.plan(3)
 
-    const render = cacheElement((name) => html`<div>${name}</div>`)
+    var render = cacheElement((name) => html`<div>${name}</div>`)
 
-    const el1 = render('mittens')
+    var el1 = render('mittens')
     t.equal(String(el1), '<div>mittens</div>', 'init render success')
 
-    const el2 = render('mittens', 'mittens')
-    const same1 = el2.isSameNode(el1)
+    var el2 = render('mittens', 'mittens')
+    var same1 = el2.isSameNode(el1)
     t.equal(same1, true, 'proxy success')
 
-    const el3 = render('scruffles', 'mittens')
+    var el3 = render('scruffles', 'mittens')
     t.equal(String(el3), '<div>scruffles</div>', 're-render success')
   })
 
   t.test('should accept a custom compare function', (t) => {
     t.plan(2)
-    const create = (name) => html`<div>${name}</div>`
-    const compare = (el) => (el === 'humans!')
-    const render = cacheElement(create, compare)
+    var create = (name) => html`<div>${name}</div>`
+    var compare = (el) => (el === 'humans!')
+    var render = cacheElement(create, compare)
 
-    const el1 = render('mittens')
+    var el1 = render('mittens')
     t.equal(String(el1), '<div>mittens</div>', 'init render success')
 
-    const el2 = render('humans!')
-    const same1 = el2.isSameNode(el1)
+    var el2 = render('humans!')
+    var same1 = el2.isSameNode(el1)
     t.equal(same1, true, 'proxy success')
   })
 })
@@ -44,29 +44,33 @@ test('cache', (t) => {
 test('widget', (t) => {
   t.test('should validate input types', (t) => {
     t.plan(1)
-    t.throws(widget.bind(null, 123), /function/)
+    t.throws(widget.bind(null, 123), /object/)
   })
 
   t.test('should render elements', (t) => {
     t.plan(3)
+    var render = createNode()
 
-    const render = widget((update) => {
-      const el = html`<div></div>`
-      update((newName) => {
-        el.innerText = newName
-      })
-      return el
-    })
+    var el1 = render('mittens')
+    t.equal(String(el1.childNodes[0].data), 'mittens', 'init render success')
 
-    const el1 = render('mittens')
-    t.equal(String(el1.innerText), 'mittens', 'init render success')
-
-    const el2 = render('snowball', 'mittens')
-    const same1 = el2.isSameNode(el1)
+    var el2 = render('snowball')
+    var same1 = el2.isSameNode(el1)
     t.equal(same1, true, 'proxy success')
 
-    const el3 = render('scruffles', 'mittens')
-    const same2 = el3.isSameNode(el1)
+    var el3 = render('scruffles')
+    var same2 = el3.isSameNode(el1)
     t.equal(same2, true, 'proxy success')
+
+    function createNode () {
+      return widget({
+        onupdate: function (el, newName) {
+          el.innerText = newName
+        },
+        render: function (name) {
+          return html`<div>${name}</div>`
+        }
+      })
+    }
   })
 })
