03b6f36c645cc692219a33141c99fe60fdb20f97
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-04-03 01:27:00 +0200

    use nanocomponent

--- index.js
+++ index.js
@@ -1,60 +1,17 @@
-var document = require('global/document')
-var assert = require('assert')
+var Nanocomponent = require('nanocomponent')
 
-var elType = 'div'
+module.exports = CacheElement
 
-module.exports = cacheElement
+function CacheElement (render) {
+  if (!(this instanceof CacheElement)) return new CacheElement(render)
+  this._handleRender = render
+}
+CacheElement.prototype = Object.create(Nanocomponent.prototype)
 
-// cache an element
-// (fn, fn) -> fn()
-function cacheElement (createNode, compare) {
-  compare = compare || defaultCompare
-
-  assert.equal(typeof createNode, 'function', 'cache-element: createNode should be an function')
-  assert.equal(typeof compare, 'function', 'cache-element: compare should be a function')
-
-  var isProxied = false
-  var element = null
-  var proxy = null
-  var _args = null
-
-  // render an element
-  // (any...) -> obj
-  return function render () {
-    var args = new Array(arguments.length)
-    for (var i = 0; i < args.length; i++) {
-      args[i] = arguments[i]
-    }
-
-    if (!element) {
-      element = createNode.apply(null, args)
-      _args = args
-      return element
-    } else {
-      var isEqual = compare(args, _args)
-      if (isEqual) {
-        if (!isProxied) {
-          proxy = document.createElement(elType)
-          proxy.isSameNode = function (el) {
-            return (el === element)
-          }
-        }
-        return proxy
-      } else {
-        element = createNode.apply(null, args)
-        isProxied = false
-        _args = args
-        return element
-      }
-    }
-  }
+CacheElement.prototype._render = function () {
+  return this._handleRender()
 }
 
-function defaultCompare (args1, args2) {
-  var length = args1.length
-  if (length !== args2.length) return false
-  for (var i = 0; i < length; i++) {
-    if (args1[i] !== args2[i]) return false
-  }
-  return true
+CacheElement.prototype._update = function () {
+  return false
 }
