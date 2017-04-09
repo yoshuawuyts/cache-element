883a3c22335dc920f6f87422f8a824ea4777dff8
Rune <runefh@gmail.com>
2017-04-09 02:06:01 +0200

    Call nanocomponent constructor (#41)
    The constructor isn't currently called, which means the element is never cached because `_hasWindow` is always undefined. See https://github.com/yoshuawuyts/nanocomponent/blob/master/index.js#L7 and https://glitch.com/edit/#!/good-crack
    
    Call nanocomponent constructor (#41)
    
    The constructor isn't currently called, which means the element is never cached because `_hasWindow` is always undefined. See https://github.com/yoshuawuyts/nanocomponent/blob/master/index.js#L7 and https://glitch.com/edit/#!/good-crack

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var Nanocomponent = require('nanocomponent')
+var assert = require('assert')
 
 module.exports = CacheElement
 
 function CacheElement (render) {
   if (!(this instanceof CacheElement)) return new CacheElement(render)
+  Nanocomponent.call(this)
+  assert.equal(typeof render, 'function', 'cache-element: render should be type function')
   this._handleRender = render
 }
 CacheElement.prototype = Object.create(Nanocomponent.prototype)
